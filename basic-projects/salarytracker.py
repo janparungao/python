22c3b75ba5c4ce1b422db73bcd504faf153d5337
janparungao <janparungao03@gmail.com>
2025-12-11 12:08:50 +0000

    Reorganize basic projects: move character.py to root and remove old README; add salarytracker.py

---
+++ basic-projects/salarytracker.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self, name, level):
+        self._name = name
+        self._level = level
+
+    def __str__(self):
+        return f"{self._name}: {self._level}"
+    
+
+charlie_brown = Employee('Charlie Brown', 'trainee')
+print(charlie_brown)
