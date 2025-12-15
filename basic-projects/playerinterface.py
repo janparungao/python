0b031894447a9a8d1c01bf44601f933d6c7d881d
janparungao <janparungao03@gmail.com>
2025-12-15 23:16:06 +0000

    Complete player interface

---
+++ basic-projects/playerinterface.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+import random
+
+class Player(ABC):
+    def __init__(self):
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
+        
+    def make_move(self):
+        position = random.choice(self.moves)
