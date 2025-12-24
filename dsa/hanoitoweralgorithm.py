9635cb7f3eb4770ca358b1963ad20f26db90e2a9
janparungao <janparungao03@gmail.com>
2025-12-24 20:24:35 +0000

    Completed Hanoi Tower Algorithm

---
+++ dsa/hanoitoweralgorithm.py
@@ -0,0 +1,30 @@
+def hanoi_solver(total_disks: int):
+    def mover(start, end):
+        disk = rods[start].pop()
+        rods[end].append(disk)
+        states.append(f"{rods['A']} {rods['B']} {rods['C']}")
+    def solver(n, start, end, spare):
+        if n == 1:
+            mover(start, end)
+            return
+        else:
+            solver(n-1, start, spare, end)
+            mover(start, end)
+            solver(n-1, spare, end, start)
+            
+    if total_disks < 1:
+        raise ValueError("n must be greater then or equal to 1")
+    # moves = (2**total_disks) - 1
+    rods = {
+        "A" : list(range(total_disks,0, -1)),
+        "B" : [],
+        "C" : []        
+    }
+    states = []
+    states.append(f"{rods['A']} {rods['B']} {rods['C']}")
+
+    solver(total_disks, "A", "C", "B")
+    return "\n".join(states)
+
+        
+print(hanoi_solver(3))
