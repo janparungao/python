cc404d67b742c0436d64feed2f4dc7fafc9ec799
janparungao <janparungao03@gmail.com>
2025-12-24 00:39:43 +0000

    Completed Luhn Algorithm

--- dsa/luhnalgorithm.py
+++ dsa/luhnalgorithm.py
@@ -1,6 +1,29 @@
 def verify_card_number(card_number: str):
-    number = list(card_number)
-    return number
+    numbers = []
+    if "-" not in card_number and " " not in card_number:
+        for c in card_number:
+            numbers.append(int(c))
+    else:
+        cleaned = card_number.replace("-","").replace(" ","")
+        for c in cleaned:
+            c = int(c)
+            numbers.append(c)
 
-verify_card_number('453914889')
-print(verify_card_number('453914889'))
+    check_digit = numbers[-1]
+    numbers = numbers[:-1]
+    numbers = numbers[::-1]
+    total = 0
+    for i in range(len(numbers)):
+        if i % 2 == 0:
+            doubled = numbers[i] * 2
+            if doubled > 9:
+                doubled -= 9
+            total += doubled
+        else:
+            total += numbers[i]
+        
+    total += check_digit
+    if total % 10 == 0:
+        return f"VALID!"
+    else:
+        return f"INVALID!"
